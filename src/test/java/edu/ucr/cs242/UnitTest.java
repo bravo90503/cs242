5e5d0e47b110d7d01a51ade2f5e65cac2fd5be7a
marlon <bravo90503@yahoo.com>
2022-02-28 10:09:39 -0800

    adjusted test to read gzip file

--- src/test/java/edu/ucr/cs242/UnitTest.java
+++ src/test/java/edu/ucr/cs242/UnitTest.java
@@ -2,97 +2,40 @@ package edu.ucr.cs242;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.net.URL;
+import java.util.zip.GZIPInputStream;
 
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import edu.ucr.cs242.repo.model.*;
+
+import edu.ucr.cs242.repo.model.Document;
+import edu.ucr.cs242.repo.model.Keyword;
 
 public class UnitTest {
 
 	private static ObjectMapper mapper = new ObjectMapper();
 
 	@Test
-	public void readFileLineByLine() {
-		URL resource = Thread.currentThread().getContextClassLoader().getResource(".");
-		try {
-			File file = new File(resource.getPath() + "part-r-00000"); // creates a new file instance
-			FileReader fr = new FileReader(file); // reads the file
-			BufferedReader br = new BufferedReader(fr); // creates a buffering character input stream
-			StringBuffer sb = new StringBuffer(); // constructs a string buffer with no characters
-			String line;
-			while ((line = br.readLine()) != null) {
-				sb.append(line); // appends line to string buffer
-				sb.append("\n"); // line feed
-			}
-			fr.close(); // closes the stream and release the resources
-			System.out.println("Contents of File: ");
-			System.out.println(sb.toString()); // returns a string that textually represents the object
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	@Test
-	public void parseJsonPortion() {
-		URL resource = Thread.currentThread().getContextClassLoader().getResource(".");
-		try {
-			File file = new File(resource.getPath() + "part-r-00000"); // creates a new file instance
-			FileReader fr = new FileReader(file); // reads the file
-			BufferedReader br = new BufferedReader(fr); // creates a buffering character input stream
-			StringBuffer sb = new StringBuffer(); // constructs a string buffer with no characters
-			String line;
-			while ((line = br.readLine()) != null) {
-				int beginIndex = line.indexOf("{");
-				sb.append(line.substring(beginIndex, line.length())); // appends line to string buffer
-				sb.append("\n"); // line feed
-			}
-			fr.close(); // closes the stream and release the resources
-			System.out.println("Json Contents of File: ");
-			System.out.println(sb.toString()); // returns a string that textually represents the object
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	@Test
-	public void parseKeyPortion() {
-		URL resource = Thread.currentThread().getContextClassLoader().getResource(".");
-		try {
-			File file = new File(resource.getPath() + "part-r-00000"); // creates a new file instance
-			FileReader fr = new FileReader(file); // reads the file
-			BufferedReader br = new BufferedReader(fr); // creates a buffering character input stream
-			StringBuffer sb = new StringBuffer(); // constructs a string buffer with no characters
-			String line;
-			while ((line = br.readLine()) != null) {
-				int beginIndex = line.indexOf("{");
-				sb.append(line.substring(0, beginIndex).trim()); // appends line to string buffer
-				sb.append("\n"); // line feed
-			}
-			fr.close(); // closes the stream and release the resources
-			System.out.println("Json Contents of File: ");
-			System.out.println(sb.toString()); // returns a string that textually represents the object
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	@Test
 	public void KeywordToMongo() {
 		URL resource = Thread.currentThread().getContextClassLoader().getResource(".");
 		try {
-			File file = new File(resource.getPath() + "part-r-00000"); // creates a new file instance
-			FileReader fr = new FileReader(file); // reads the file
-			BufferedReader br = new BufferedReader(fr); // creates a buffering character input stream
+			FileInputStream stream = new FileInputStream(resource.getPath() + "part-r-00000.gz"); // creates a new file
+			// instance
+			GZIPInputStream gzipstream = new GZIPInputStream(stream);
+			Reader decoder = new InputStreamReader(gzipstream, "UTF-8");
+			BufferedReader buffered = new BufferedReader(decoder);
 			StringBuffer keysb = new StringBuffer(); // constructs a string buffer with no characters
 			StringBuffer documents = new StringBuffer(); // constructs a string buffer with no characters
 			String line;
-			while ((line = br.readLine()) != null) {
+			while ((line = buffered.readLine()) != null) {
 
 				int beginIndex = line.indexOf("{");
 				String key = line.substring(0, beginIndex).trim();
@@ -103,7 +46,7 @@ public class UnitTest {
 					break;
 				}
 			}
-			fr.close(); // closes the stream and release the resources
+			buffered.close(); // closes the stream and release the resources
 			System.out.println("Json Contents of File: ");
 			System.out.println(keysb.toString()); // returns a string that textually represents the object
 			System.out.println(documents.toString()); // returns a string that textually represents the object
@@ -111,14 +54,15 @@ public class UnitTest {
 			Keyword keyword = mapper.readValue(documents.toString(), Keyword.class);
 			keyword.setKey(keysb.toString());
 			Double maxScore = null;
-			Assert.assertTrue(String.format("expected 1001, got %d", keyword.getDocuments().size()), keyword.getDocuments().size() == 1001);
+			Assert.assertTrue(String.format("expected 1001, got %d", keyword.getDocuments().size()),
+					keyword.getDocuments().size() == 1001);
 			for (Document document : keyword.getDocuments()) {
-				System.out.println("docId="+document.getDocId() + ", score=" +document.getScore());
+				System.out.println("docId=" + document.getDocId() + ", score=" + document.getScore());
 				if (maxScore == null) {
 					maxScore = new Double(document.getScore());
-					//continue;
+					// continue;
 				}
-				
+
 				Assert.assertTrue(maxScore >= document.getScore());
 			}
 
