d6e44e82837e40c200d1886e92fead59dd446ed8
marlon <bravo90503@yahoo.com>
2022-03-02 10:27:15 -0800

    moving inverted index parser to hadoop service

--- src/main/java/edu/ucr/cs242/service/HadoopSearch.java
+++ src/main/java/edu/ucr/cs242/service/HadoopSearch.java
@@ -1,12 +1,24 @@
 package edu.ucr.cs242.service;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
+import java.util.zip.GZIPInputStream;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ResourceUtils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import edu.ucr.cs242.repo.model.Document;
 import edu.ucr.cs242.repo.model.Keyword;
@@ -14,6 +26,60 @@ import edu.ucr.cs242.web.dto.DocumentDto;
 
 @Service
 public class HadoopSearch {
+
+	private static ObjectMapper mapper = new ObjectMapper();
+
+	public PriorityQueue<DocumentDto> searchByContent(String content, int howMany) {
+		PriorityQueue<DocumentDto> topDocs = new PriorityQueue<>();
+		try {
+			List<Keyword> keywords = getKeywords(content.split(" "));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return topDocs;
+	}
+
+	public List<Keyword> getKeywords(String[] tokens) throws IOException {
+		Map<String, String> tokenMap = new HashMap<>();
+		for (String token : tokens) {
+			tokenMap.put(token, token);
+		}
+		File file = ResourceUtils.getFile("classpath:" + "part-r-00000.gz");
+		FileInputStream stream = new FileInputStream(file); // creates a new file
+		// instance
+		GZIPInputStream gzipstream = new GZIPInputStream(stream);
+		Reader decoder = new InputStreamReader(gzipstream, "UTF-8");
+		BufferedReader buffered = new BufferedReader(decoder);
+		StringBuilder[] keys = new StringBuilder[tokens.length];
+		StringBuilder[] docs = new StringBuilder[tokens.length];
+		String line;
+		int index = 0;
+		while ((line = buffered.readLine()) != null) {
+			int beginIndex = line.indexOf("{");
+			String key = line.substring(0, beginIndex).trim();
+			if (StringUtils.isNotBlank(key) && tokenMap.get(key) != null) {
+				StringBuilder keysb = new StringBuilder();
+				StringBuilder doc = new StringBuilder();
+				keysb.append(key); // appends line to string buffer
+				doc.append(line.substring(beginIndex, line.length()));
+				keys[index] = keysb;
+				docs[index] = doc;
+				index++;
+			}
+		}
+		buffered.close(); // closes the stream and release the resources
+
+		List<Keyword> keywords = new ArrayList<>(keys.length);
+		for (int i = 0; i < keys.length; i++) {
+			Keyword keyword = mapper.readValue(docs[i].toString(), Keyword.class);
+			keyword.setKey(keys[i].toString());
+			keywords.add(keyword);
+		}
+
+		return keywords;
+	}
 
 	public int findTopDocuments(List<Keyword> keywords, int howMany, PriorityQueue<DocumentDto> topDocs) {
 		Document[] iterationDocs = new Document[keywords.size()];
--- src/test/java/edu/ucr/cs242/UnitTest.java
+++ src/test/java/edu/ucr/cs242/UnitTest.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.PriorityQueue;
 import java.util.zip.GZIPInputStream;
@@ -79,75 +78,12 @@ public class UnitTest {
 
 	@Test
 	public void KeywordScoring() {
-		PriorityQueue<edu.ucr.cs242.web.dto.DocumentDto> finalScores = new PriorityQueue<>();
-
-		URL resource = Thread.currentThread().getContextClassLoader().getResource(".");
 		try {
-			FileInputStream stream = new FileInputStream(resource.getPath() + "part-r-00000.gz"); // creates a new file
-			// instance
-			GZIPInputStream gzipstream = new GZIPInputStream(stream);
-			Reader decoder = new InputStreamReader(gzipstream, "UTF-8");
-			BufferedReader buffered = new BufferedReader(decoder);
-			StringBuffer information = new StringBuffer(); // constructs a string buffer with no characters
-			StringBuffer informations = new StringBuffer(); // constructs a string buffer with no characters
-			StringBuffer retrieval = new StringBuffer(); // constructs a string buffer with no characters
-			StringBuffer retrievals = new StringBuffer(); // constructs a string buffer with no characters
-			String line;
-			while ((line = buffered.readLine()) != null) {
-
-				int beginIndex = line.indexOf("{");
-				String key = line.substring(0, beginIndex).trim();
-				if (StringUtils.isNotBlank(key) && key.equals("information")) {
-					information.append(key); // appends line to string buffer
-					informations.append(line.substring(beginIndex, line.length()));
-				}
-				if (StringUtils.isNotBlank(key) && key.equals("retrieval")) {
-					retrieval.append(key); // appends line to string buffer
-					retrievals.append(line.substring(beginIndex, line.length()));
-				}
-			}
-			buffered.close(); // closes the stream and release the resources
-
-			Keyword informationKeyword = mapper.readValue(informations.toString(), Keyword.class);
-			informationKeyword.setKey(information.toString());
-			Double maxScore = null;
-			Assert.assertTrue(String.format("expected 1001, got %d", informationKeyword.getDocuments().size()),
-					informationKeyword.getDocuments().size() == 1001);
-			for (Document document : informationKeyword.getDocuments()) {
-				// System.out.println("docId=" + document.getDocId() + ", score=" +
-				// document.getScore());
-				if (maxScore == null) {
-					maxScore = new Double(document.getScore());
-					// continue;
-				}
-
-				Assert.assertTrue(maxScore >= document.getScore());
-			}
-
-			Keyword retrievalKeyword = mapper.readValue(retrievals.toString(), Keyword.class);
-			retrievalKeyword.setKey(information.toString());
-			maxScore = null;
-			Assert.assertTrue(String.format("expected 319, got %d", retrievalKeyword.getDocuments().size()),
-					retrievalKeyword.getDocuments().size() == 319);
-			for (Document document : retrievalKeyword.getDocuments()) {
-				// System.out.println("docId=" + document.getDocId() + ", score=" +
-				// document.getScore());
-				if (maxScore == null) {
-					maxScore = new Double(document.getScore());
-					// continue;
-				}
-
-				Assert.assertTrue(maxScore >= document.getScore());
-			}
-
+			String[] tokens = new String[] { "information", "retrieval" };
+			// keywords
+			List<Keyword> keywords = hadoopSearchService.getKeywords(tokens);
 			// documents to return
 			int howMany = 320;
-
-			// keywords
-			List<Keyword> keywords = new ArrayList<>();
-			keywords.add(informationKeyword);
-			keywords.add(retrievalKeyword);
-
 			// sorted by highest score
 			PriorityQueue<DocumentDto> topDocs = new PriorityQueue<>();
 
